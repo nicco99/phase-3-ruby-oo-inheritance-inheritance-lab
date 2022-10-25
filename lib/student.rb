11ee59651bb7ed12b36824dc5b18b76f45ca43a7
Nicholas Njeru <nicholasnjeru917@gmail.com>
2022-10-26 00:14:17 +0300

    completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+require_relative'./user'
+class Student < User
+attr_accessor :first_name, :last_name
+attr_reader :knowledge
+def initialize arr=[]
+    @knowledge = arr
+end
+
+def learn string
+@knowledge << string
+end 
 
 end
